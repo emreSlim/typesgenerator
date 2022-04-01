b98c129dedcb64d6330a33a488a06db3bd0c72cb
emreSlim <emraanqureshi9@gmail.com>
2022-04-01 17:25:11 +0530

    added interface file creating function

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,69 +1,60 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs'
-import {getInterface} from '../lib/getInterface'
+import * as fs from 'fs';
+import { getInterface } from '../lib/getInterface';
+import {  createInterfaceFile, logError } from '../utils';
+import * as path from 'path';
 
+const args: string[] = process.argv.slice(2);
 
-const args:string[] = process.argv.slice(2)
+let sourcePath: string|undefined, targetPath: string|undefined, typeName: string|undefined;
 
-let sourcePath,targetPath,typeName :string;
+for (let i = 0; i < args.length; i++) {
+  let arg = args[i];
+  const nextArg = args[i + 1];
 
-for(let i=0;i<args.length;i++){
-  let arg = args[i]
-  const nextArg = args[i+1]
-
-  if(isArgumentKey(arg) && !isArgumentKey(nextArg)){
+  if (isArgumentKey(arg) && !isArgumentKey(nextArg)) {
     arg = arg.toLowerCase();
-      switch(arg){
-        case '-s':
-        case '--source-path':
-          sourcePath = nextArg;break;
-        case '-t':
-        case '--target-path':
-          targetPath = nextArg;break;
-          case '-n':
-            case '--type-name':
-              typeName = nextArg;break;
-      }
-      i++;//skip next
-  }else{continue}
-}
-
-function isArgumentKey(str:string){
-  return str.startsWith('-')
-}
-
-try{
-if(sourcePath){
-
-  if(fs.existsSync(sourcePath)){
-   fs.readFile(sourcePath,'utf-8',(err:NodeJS.ErrnoException,data:string)=>{
-      if(err){
-        throw err;
-      }else{
-        !typeName && logWarning('Type Name is not given. Type generated with dummy name.')
-        const string = getInterface(JSON.parse(data),typeName)
-        console.log(string)
-      }
-   })
-    
-  }else{
-    throw new Error("given source path doesn't exist");
+    switch (arg) {
+      case '-s':
+      case '--source-path':
+        sourcePath = nextArg;
+        break;
+      case '-t':
+      case '--target-path':
+        targetPath = nextArg;
+        break;
+      case '-n':
+      case '--type-name':
+        typeName = nextArg;
+        break;
+    }
+    i++; //skip next
+  } else {
+    continue;
   }
-}else{
-  throw new Error("source path to JSON not given");
-}
-}catch(e){
-  logError(e.message)
 }
 
-
-//  suffix \x1b[0m for reset,  \x1b[4 for bg and \x1b[3 for fg color
-
-function logWarning(...args:any){
-    console.log('\x1b[33m','Warning:',...args,'\x1b[0m')
-}
-
-function logError(...args:any){
-  console.log('\x1b[41m','\x1b[37m','Error:',...args,'\x1b[0m')
+try {
+  if (sourcePath) {
+    if (fs.existsSync(sourcePath)) {
+      fs.readFile(
+        sourcePath,
+        'utf-8',
+        (err: NodeJS.ErrnoException, data: string) => {
+          if (err) {
+            throw err;
+          } else {
+            createInterfaceFile(JSON.parse(data),typeName,sourcePath,targetPath)
+          }
+        }
+      );
+    } else {
+      throw new Error("given source path doesn't exist");
+    }
+  } else {
+    throw new Error('source path to JSON not given');
+  }
+} catch (e) {
+  logError(e.message);
 }
