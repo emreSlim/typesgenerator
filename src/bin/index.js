f1f8dff3960ec7bf908d3a76a2a556c38ff1bdeb
emreSlim <emraanqureshi9@gmail.com>
2022-04-01 12:50:37 +0530

    added configs for webpack

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,3 +1,17 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+
 const commands = process.argv.slice(2)
+
+const [typeName,sourcePath,targetPath] = commands;
+
+if(sourcePath){
+
+  if(fs.existsSync(sourcePath)){
+    console.log('exists')
+  }else{
+    console.log('doesnt exist')
+  }
+    
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,12 @@ const path = require('path')
 module.exports = {
   mode:'production',
   entry:{
-    lib:'/src/lib/index.js',
+    dist:'/src/lib/index.js',
     bin:'/src/bin/index.js'
   },
   output:{
-    path:path.resolve(__dirname,'dist'),
-    filename:'[name].js'
-  }
+    path:__dirname,
+    filename:"[name]/index.js"
+  },
+  target:'node'
 };
