45c200d1b040df8b955add3c173a5771345e8e4d
MobackImranQureshi <73607413+emreSlim@users.noreply.github.com>
2022-10-25 02:41:39 +0530

    readme update

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Typesgenerator
 
-Typesgenerator helps in creating typescript types from JSON or JavaScript-object. 
+Typesgenerator helps in creating typescript types/interfaces from JSON or JavaScript-object.
 
 ## Installation and Usage
 
 - ### As Package: To use a function for printing type:
+
 ```bash
 npm i -D typesgenerator
 ```
@@ -12,32 +13,35 @@ npm i -D typesgenerator
 Below is usage example for printing the type from the javascript/json object:
 
 ```js
-import {printType} from 'typesgenerator'
+import { printType } from 'typesgenerator';
 
 const object = {
-  name:"john doe",
-  hobbies:[{name:"coding",outdoor:false},{name:"Cycling",outdoor:true}],
-  age:35,
-}
+  name: 'john doe',
+  hobbies: [
+    { name: 'coding', outdoor: false },
+    { name: 'Cycling', outdoor: true },
+  ],
+  age: 35,
+};
 
-printType(object);
+printType(object, 'Person');
 
 //console output:
 /*
-export type PersonHobbiesItem = {
+export type PersonHobbie = {
   name: string;
   outdoor: boolean;
 };
 
 export type Person = {
   name: string;
-  hobbies: PersonHobbiesItem[];
+  hobbies: PersonHobbie[];
   age: number;
 };
 */
 ```
 
--  ### As CLI: To create a typescript file from JSON:
+- ### As CLI: To create a typescript file from JSON:
 
 ```bash
 npm i -g typesgenerator
@@ -49,13 +53,12 @@ Below is the usage example that will create a new file containing types:
 tg -n <Type name> -s <path-to-json> -t <path-to-target-directory>
 ```
 
-
 Below are the suppored arguments:
-- `-n` or `-N` or `--type-name` :  Name to be given to type
+
+- `-n` or `-N` or `--type-name` : Name to be given to type
 - `-s` or `-S` or `--source-path` : path to the json
 - (optional )`-t` or `-T` or `--target-path` : path to the type-file to be generated
 
-
-
 GitHub: https://github.com/emreSlim/typescript-types-generator
+
 Issues: https://github.com/emreSlim/typescript-types-generator/issues
--- test.mjs
+++ test.mjs
@@ -1,124 +1,14 @@
-import { printType } from './dist/lib/index.js';
+import lib from './dist/lib/index.js';
+
+const { printType } = lib;
 
 const o = {
-  "jobCode": "ITPM04",
-  "vertices": [
-    {
-      "nextBestMove": {
-        "fitScore": 0.2621,
-        "id": "ITPM10",
-        "name": "Product Marketing Manager III"
-      },
-      "id": "ITPM03",
-      "jobCode": "ITPM03",
-      "name": "Marketing - General - Functional Manager of Managers II",
-      "title": null,
-      "description": "sample description",
-      "function": "IT & Technology",
-      "subFunction": "Product Management",
-      "grade": "18",
-      "level": "Mid Level Manager",
-      "isExecutive": false,
-      "noofEmployees": 0,
-      "noofPositions": 0
-    },
-    {
-      "nextBestMove": {
-        "fitScore": 0.4731,
-        "id": "ITUX03",
-        "name": "User Experience (UX) Manager III"
-      },
-      "id": "ITPM10",
-      "jobCode": "ITPM10",
-      "name": "Product Marketing Manager III",
-      "title": null,
-      "description": "sample description",
-      "function": "IT & Technology",
-      "subFunction": "Product Management",
-      "grade": "18",
-      "level": "Individual Contributor",
-      "isExecutive": false,
-      "noofEmployees": 0,
-      "noofPositions": 0
-    },
-    {
-      "nextBestMove": {
-        "fitScore": 0.7231,
-        "id": "ITPM03",
-        "name": "Marketing - General - Functional Manager of Managers II"
-      },
-      "id": "ITPM04",
-      "jobCode": "ITPM04",
-      "name": "Marketing - General - Functional Manager of Managers II",
-      "title": null,
-      "description": "sample description",
-      "function": "IT & Technology",
-      "subFunction": "Product Management",
-      "grade": "18",
-      "level": "Mid Level Manager",
-      "isExecutive": false,
-      "noofEmployees": 0,
-      "noofPositions": 0
-    },
-    {
-      "nextBestMove": null,
-      "id": "ITUX03",
-      "jobCode": "ITUX03",
-      "name": "User Experience (UX) Manager III",
-      "title": null,
-      "description": "sample description",
-      "function": "IT & Technology",
-      "subFunction": "User Experience Design",
-      "grade": "19",
-      "level": "Front Line Manager",
-      "isExecutive": false,
-      "noofEmployees": 0,
-      "noofPositions": 0
-    }
+  name: 'john doe',
+  hobbies: [
+    { name: 'coding', outdoor: false },
+    { name: 'Cycling', outdoor: true },
   ],
-  "edges": [
-    {
-      "compScore": 0.4731,
-      "transitionProbability": 0.00028548,
-      "id": "ITPM10_ITUX03",
-      "source": "ITPM10",
-      "target": "ITUX03"
-    },
-    {
-      "compScore": 0.7231,
-      "transitionProbability": 1.0,
-      "id": "ITPM04_ITPM03",
-      "source": "ITPM04",
-      "target": "ITPM03"
-    },
-    {
-      "compScore": 0.2621,
-      "transitionProbability": 0.10670205999999999,
-      "id": "ITPM04_ITPM10",
-      "source": "ITPM04",
-      "target": "ITPM10"
-    },
-    {
-      "compScore": 0.2621,
-      "transitionProbability": 0.10670205999999999,
-      "id": "ITPM03_ITPM10",
-      "source": "ITPM03",
-      "target": "ITPM10"
-    }
-  ],
-  "organizationId": "30",
-  "metadata": {
-    "functions": ["IT & Technology"],
-    "subFunctions": ["Product Management", "User Experience Design"],
-    "levels": [
-      "Front Line Manager",
-      "Individual Contributor",
-      "Mid Level Manager"
-    ],
-    "grades": ["18", "19"]
-  }
-}
+  age: 35,
+};
 
-
-
-printType(o, 'NG');
+printType(o, 'Person');
